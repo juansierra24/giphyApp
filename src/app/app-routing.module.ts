06adb37cc159ef4360595d460228d2a9f584cca3
juansierra24 <carlos.sierratrujillo@gmail.com>
2023-06-06 07:25:53 -0500

    delete unused code and update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'giphy', pathMatch: 'full'},
   {
     path: 'giphy',
     loadChildren: () => import('./giphy/giphy.module').then(m => m.GiphyModule),
